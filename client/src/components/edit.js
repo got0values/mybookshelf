a23a6fca71444ac99e9d2e1d3589f1e0ae43738d
got0values <gotnovalues@gmail.com>
2021-10-24 18:19:28 +0000

    fixed update

--- client/src/components/edit.js
+++ client/src/components/edit.js
@@ -17,47 +17,49 @@ const Edit = (props) => {
   let {id} = useParams(); //gets the url param
   let history = useHistory();
 
-  const getBook = async () => {
-    try {
-      await axios
-      .get(props.server + "/book/" + id)
-      .then((response) => {
-        setBook({
-          bookISBN: response.data.book_ISBN,
-          bookTitle: response.data.book_title,
-          bookAuthor: response.data.book_author,
-          bookRating: response.data.book_rating,
-          bookNotes: response.data.book_notes
-        })
-      })
-    }catch(e) {
-      console.log(e);
-    }
-  }
-
-  const getThumb = async () => {
-    if (book.bookISBN !== "") {
-      try {
-        await axios
-        .get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + book.bookISBN)
-        .then((response) => {
-          if (response.data.items !== undefined) {
-            setBook({ ...book,
-              bookThumbURL: response.data.items[0].volumeInfo.imageLinks.thumbnail,
-              bookDescription: response.data.items[0].volumeInfo.description
-            });
-          }
-        })
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  }
-
   useEffect(()=> {
+    const getThumb = async () => {
+        if (book.bookISBN !== "") {
+            try {
+                await axios
+                .get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + book.bookISBN)
+                .then((response) => {
+                if (response.data.items !== undefined) {
+                    console.log(response.data.items[0].volumeInfo.imageLinks.thumbnail);
+                    setBook({ ...book,
+                    bookThumbURL: response.data.items[0].volumeInfo.imageLinks.thumbnail,
+                    bookDescription: response.data.items[0].volumeInfo.description
+                    });
+                }
+                })
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+
+    const getBook = async () => {
+        console.log(props.server)        
+        try {
+          await axios
+          .get(props.server + "/book/" + id)
+          .then((response) => {
+            setBook({
+              bookISBN: response.data.book_ISBN,
+              bookTitle: response.data.book_title,
+              bookAuthor: response.data.book_author,
+              bookRating: response.data.book_rating,
+              bookNotes: response.data.book_notes
+            })
+          })
+        }catch(e) {
+          console.log(e);
+        }
+    }
     getBook();
     getThumb();
-  }, [id, book.bookISBN])
+        
+    }, [id, props.server])
 
   const onChangeBookISBN = (e) => {
     setBook({
@@ -104,20 +106,12 @@ const Edit = (props) => {
     // This will send a post request to update the data in the database.
     axios
       .post(
-        "http://localhost:5000/update/" + id,
+        props.server + "/update/" + id,
         newEditedBook
       )
       .then((res) => console.log(res.data));
  
     history.push("/view/" + id);
-  }
-
-  let BookThumb = () => {
-    return (
-        <>
-            <img src={book.bookThumbURL} alt="bookthumb" className="float-left mr-3 mb-1"/>
-        </>
-    )
   }
 
   let BookDescription = () => {
@@ -132,7 +126,6 @@ const Edit = (props) => {
     return (
       <div className="row">
           <div className="col">
-            <BookThumb/>
             <BookDescription/>
         </div>
       </div>
@@ -142,7 +135,6 @@ const Edit = (props) => {
   return (
     <div className="container w-75 fixed-right">
       <h3 className="card-title d-flex flex-row align-items-center justify-content-center bg-white mt-3">Update Book: {book.bookTitle}</h3>
-      {console.log(book.bookISBN)}
       {book.bookISBN !== "" ? <BookInfo/> : ""}
       <hr/>
       <form onSubmit={onSubmit}>
